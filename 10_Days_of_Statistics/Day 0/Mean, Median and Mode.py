5920fb43e0386a796f9cc6df360e319e2250ed6f
yashml <yashml@outlook.com>
2020-04-09 20:28:35 +0530

    Added Comments

--- 10_Days_of_Statistics/Day 0/Mean, Median and Mode.py
+++ 10_Days_of_Statistics/Day 0/Mean, Median and Mode.py
@@ -1,9 +1,12 @@
+# Importing the libraries
 import numpy as np
 from scipy import stats
 
+# Getting the input
 no_of_elements = int(input())
 elements = list(map(int, input().split()))
 
+# Printing the output
 print(np.mean(elements))
 print(np.median(elements))
 print(int(stats.mode(elements)[0]))
--- 10_Days_of_Statistics/Day 0/Weighted Mean.py
+++ 10_Days_of_Statistics/Day 0/Weighted Mean.py
@@ -1,3 +1,4 @@
+# Getting the input
 no_of_elements = int(input())
 elements = list(map(int, input().split()))
 element_weights = list(map(int, input().split()))
@@ -7,4 +8,5 @@ sum_elements = 0
 for i in range(no_of_elements):
     sum_elements += elements[i]*element_weights[i]
 
+# Printing the output
 print(round(sum_elements/sum(element_weights), 1))
