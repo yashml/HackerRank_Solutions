9ce281097480007443c091d6a294ab065f4bf068
yashml <yashml@outlook.com>
2020-04-28 23:47:45 +0530

    Completed Day 5: C++

---
+++ 30_Days_of_Code/Day 5/Loops.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+     for (int i = 1; i < 11; i++) {
+        printf("%i x %i = %i\n", n, i, n * i);
+    }
+
+
+    return 0;
+}
