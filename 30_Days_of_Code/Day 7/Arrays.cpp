1e8a7407c163d6a446be1355fff4a36497448587
yashml <yashml@outlook.com>
2020-04-30 23:05:53 +0530

    Completed Day 7: C++

---
+++ 30_Days_of_Code/Day 7/Arrays.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    reverse(arr, arr+n);
+
+    for (int i = 0; i < ; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
