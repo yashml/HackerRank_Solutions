2f0667148d66b863643a70ab5283c86c91e0d442
yashml <yashml@outlook.com>
2020-05-10 22:19:40 +0530

    Completed Day 16: C++

---
+++ 30_Days_of_Code/Day 16/Exceptions - String to Integer.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    try {
+        string str;
+        cin>>str;
+
+        int num=stoi(str);
+        cout<<num;
+    }
+    
+    catch (exception) {
+        cout<<"Bad String";
+    }
+
+}
